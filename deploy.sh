f5c871da126d650e375c505f3a55fc540f0a51db
wenziyue <wenziyue@mifengkong.cn>
2018-02-11 11:07:40 +0800

    auto commit

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 git add .
 git commit -m"auto commit"
-git push origin dev
+git push origin master
 
 ssh root@testonly.fun >/dev/null 2>&1 << eeooff
     cd $GOPATH/src/gbuyapi
-    if [ -f .pid ]; then
+    if [ -f ".pid" ]; then
         cat .pid | xargs kill -9
     fi
     git pull origin master
