64f1ea923330bdd81f33564c15bceed92df59514
wenziyue <wenziyue@mifengkong.cn>
2018-02-11 15:36:26 +0800

    auto commit

--- controllers/orders.go
+++ controllers/orders.go
@@ -6,3 +6,13 @@ type OrderController struct {
 	beego.Controller
 }
 
+
+// @Title CreateOrder
+// @Description 用户下单，创建订单
+// @Param	body	body	models.Orders	true	"订单信息"
+// @Success 200 {int} models.Orders.Id
+// @Failure 210 请求出错
+// @router / [post]
+func (this *OrderController) CreateOrder() {
+
+}
